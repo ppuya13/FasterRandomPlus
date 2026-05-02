0372d52072de0c08e67316a4dafa22d9e1140fdb
Balensiad <ppuya13@gmail.com>
2026-05-02 12:26:04 +0900

    샤롯용... 임시

--- Source/RaceFix.cs
+++ Source/RaceFix.cs
@@ -12,6 +12,7 @@ namespace FasterRandomPlus.Source
                 return;
 
             ApplyRatkin(pawn);
+            // ApplyCharlotte(pawn);
         }
 
         #region Ratkin
@@ -33,5 +34,28 @@ namespace FasterRandomPlus.Source
         }
 
         #endregion
+        
+        // #region Charlotte
+        //
+        // //샤롯에게 고통의 환희 특성 및 전용 능력 추가
+        // private static void ApplyCharlotte(Pawn pawn)
+        // {
+        //     if (pawn.genes?.Xenotype?.defName != "Charlotte_Xenotype")
+        //         return;
+        //
+        //     var painTrait = DefDatabase<TraitDef>.GetNamedSilentFail("Charlotte_PainEcstasyTrait");
+        //     if (painTrait != null && !pawn.story.traits.HasTrait(painTrait))
+        //         pawn.story.traits.GainTrait(new Trait(painTrait));
+        //
+        //     var surgeAbility = DefDatabase<AbilityDef>.GetNamedSilentFail("Charlotte_CrimsonSurge");
+        //     if (surgeAbility != null && pawn.abilities.GetAbility(surgeAbility) == null)
+        //         pawn.abilities.GainAbility(surgeAbility);
+        //
+        //     var summonAbility = DefDatabase<AbilityDef>.GetNamedSilentFail("Charlotte_SummonFamiliar");
+        //     if (summonAbility != null && pawn.abilities.GetAbility(summonAbility) == null)
+        //         pawn.abilities.GainAbility(summonAbility);
+        // }
+        //
+        // #endregion
     }
 }
